00405654b582d913eb4c4b9634bfc32e288e9bed
Andrew Magdy <146522179+Devandrew45@users.noreply.github.com>
2024-09-08 14:38:16 +0300

    Delete app/src/main/java/com/news/task/newsapp/app/ui/navigation/Screens.kt

--- app/src/main/java/com/news/task/newsapp/app/ui/navigation/Screens.kt
+++
@@ -1,8 +0,0 @@
-package com.news.task.newsapp.app.ui.navigation
-
-sealed class Screens(val route: String) {
-    object Home : Screens("HomeNewsScreen")
-    object Detail : Screens("ArticleDetails/{message}") {
-        fun createRoute(message: String) = "ArticleDetails/$message"
-    }
-}
